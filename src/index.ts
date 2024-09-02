6aee58509a3593cfd86acd800bba01c5a352c0ce
Máté Homolya <mate.homolya@gmail.com>
2024-09-02 11:19:00 -0700

    Export type from auth

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * from "./api";
 export * from "./types";
 export * from "./trade/types";
-export { isValidationError } from "@crypticorn-ai/auth-service";
+export { isValidationError, ValidationErrorData } from "@crypticorn-ai/auth-service";
