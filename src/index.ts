0e4a45c3ed88406ee436b18053174d65d395ad05
timonrieger <mail@timonrieger.de>
2025-04-28 11:57:32 +0200

    update klines package

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,12 @@ export * from "./api";
 export * from "./types";
 // https://stackoverflow.com/questions/41293108/how-to-do-re-export-with-overrides
 
+// common 
+export * from "./common/enums";
+export * from "./common/exceptions";
+export * from "./common/errors";
+
+
 // trade
 export * from "./trade/schemas.gen";
 export * from "./trade/types.gen";
@@ -11,24 +17,22 @@ import { HTTPValidationError, PingError, PingResponse } from "./pay/types.gen";
 export * from "./pay/schemas.gen";
 export * from "./pay/types.gen";
 export { HTTPValidationErrorSchema, ValidationErrorSchema }; // pay
-// klines
-import { Exchange, MarketType, BaseResponse_HealthCheckResponse_, BaseResponse_List_str__, HealthCheckResponse, ErrorResponse } from "./klines/types.gen";
-import { ExchangeSchema, MarketTypeSchema, HealthCheckResponseSchema, BaseResponse_List_str__Schema, BaseResponse_HealthCheckResponse_Schema, ErrorResponseSchema } from "./klines/schemas.gen";
-export * from "./klines/schemas.gen";
-export * from "./klines/types.gen";
-export { ExchangeSchema, Exchange, MarketTypeSchema, MarketType, HealthCheckResponseSchema, BaseResponse_HealthCheckResponse_, BaseResponse_List_str__Schema, BaseResponse_HealthCheckResponse_Schema, BaseResponse_List_str__, HealthCheckResponse, ErrorResponse, ErrorResponseSchema }; // klines
 // sentiment
-import { BaseResponse_HealthCheckResponse_ as SentimentHealthCheckResponse } from "./sentiment/types.gen";
-import { BaseResponse_HealthCheckResponse_Schema as SentimentHealthCheckResponseSchema } from "./sentiment/schemas.gen";
 export * from "./sentiment/schemas.gen";
 export * from "./sentiment/types.gen";
-export { SentimentHealthCheckResponseSchema, SentimentHealthCheckResponse }; // sentiment
 // metrics
-import { BaseResponse_HealthCheckResponse_ as MetricsHealthCheckResponse } from "./metrics/types.gen";
-import { BaseResponse_HealthCheckResponse_Schema as MetricsHealthCheckResponseSchema } from "./metrics/schemas.gen";
+import { ApiErrorIdentifierSchema, ApiErrorLevelSchema, ApiErrorTypeSchema, MarketTypeSchema } from "./metrics/schemas.gen";
+import { ApiErrorIdentifier, ApiErrorLevel, ApiErrorType, MarketType } from "./metrics/types.gen";
 export * from "./metrics/schemas.gen";
 export * from "./metrics/types.gen";
-export { MetricsHealthCheckResponseSchema, MetricsHealthCheckResponse }; // metrics
+export { ApiErrorIdentifierSchema, ApiErrorLevelSchema, ApiErrorTypeSchema, MarketTypeSchema, ApiErrorIdentifier, ApiErrorLevel, ApiErrorType, MarketType }; // metrics
+// klines
+import { ApiErrorIdentifierSchema as KlinesApiErrorIdentifierSchema, ApiErrorLevelSchema as KlinesApiErrorLevelSchema, ApiErrorTypeSchema as KlinesApiErrorTypeSchema, MarketTypeSchema as KlinesMarketTypeSchema } from "./klines/schemas.gen";
+import { ApiErrorIdentifier as KlinesApiErrorIdentifier, ApiErrorLevel as KlinesApiErrorLevel, ApiErrorType as KlinesApiErrorType, MarketType as KlinesMarketType } from "./klines/types.gen";
+export * from "./klines/schemas.gen";
+export * from "./klines/types.gen";
+export { KlinesApiErrorIdentifierSchema, KlinesApiErrorLevelSchema, KlinesApiErrorTypeSchema, KlinesMarketTypeSchema, KlinesApiErrorIdentifier, KlinesApiErrorLevel, KlinesApiErrorType, KlinesMarketType }; // klines
+
 export { HTTPValidationError, PingError, PingResponse };
 export {
   isValidationError,
